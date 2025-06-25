6aeafb98055342444f060de7d2a0263455079a11
Ertuğrul Köse <ertugrulkose1235@gmail.com>
2025-06-25 07:34:51 +0000

    links updated

--- app.js
+++ app.js
@@ -2,11 +2,25 @@ const express = require('express');
 
 const app = express();
 
+//Template Engine
+app.set('view engine', 'ejs');
+
+//MiddleWare
+app.use(express.static('public'));
+
 const PORT = 3000;
 app.listen(PORT, () => {
 	console.log(`App Started on port ${PORT}`);
 });
 
 app.get('/', async (req, res) => {
-	res.send('index Sayfasi');
+	res.status(200).render("index", {
+		page_name: "index"
+	})
 });
+
+app.get("/about", async (req, res)=>{
+	res.status(200).render('about', {
+		page_name: 'about'
+	});
+})
