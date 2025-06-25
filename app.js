d55560fb5fea8171e898c7de2f6ca7f347fe23a0
Ertuğrul Köse <ertugrulkose1235@gmail.com>
2025-06-25 08:05:57 +0000

    MVC struct created

--- app.js
+++ app.js
@@ -2,25 +2,18 @@ const express = require('express');
 
 const app = express();
 
+const pageRouter = require("./routers/pageRoute");
+
 //Template Engine
 app.set('view engine', 'ejs');
 
 //MiddleWare
 app.use(express.static('public'));
 
+//Routes
+app.use("/", pageRouter);
+
 const PORT = 3000;
 app.listen(PORT, () => {
 	console.log(`App Started on port ${PORT}`);
 });
-
-app.get('/', async (req, res) => {
-	res.status(200).render("index", {
-		page_name: "index"
-	})
-});
-
-app.get("/about", async (req, res)=>{
-	res.status(200).render('about', {
-		page_name: 'about'
-	});
-})
